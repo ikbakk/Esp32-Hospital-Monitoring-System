f1ac11d608f2090b5337e43531a530f29b5c43ab
ikbakk <iqbalfirdaus05@gmail.com>
2024-06-05 12:19:01 +0800

    change date format

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -1,9 +1,9 @@
 const dateFormatter = (timestamp: number, withDaysName: boolean) => {
   const date = new Date(timestamp);
-  const day = date.getDay();
-  const dd = date.getDate();
-  const mm = date.getMonth();
-  const yyyy = date.getFullYear().toString().slice(2);
+  const day = date.getUTCDay();
+  const dd = date.getUTCDate();
+  const mm = date.getUTCMonth();
+  const yyyy = date.getUTCFullYear().toString().slice(2);
 
   const dayFormat = (day: number) => {
     const days = [
@@ -13,7 +13,7 @@ const dateFormatter = (timestamp: number, withDaysName: boolean) => {
       'Rabu',
       'Kamis',
       'Jumat',
-      'Sabtu'
+      'Sabtu',
     ];
     return days[day];
   };

8e7da41ba388f641cbbde2776fe6a27a704ac198
ikbakk <ikbakfir@gmail.com>
2025-08-22 09:05:41 +0800

    clean files

--- components/AddForm.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-interface AddFormProps {}
-
-const AddForm: FC<AddFormProps> = () => {
-  return <div>AddForm</div>;
-};
-
-export default AddForm;
--- components/RoomCard/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-interface LoadingProps {}
-
-const Loading: FC<LoadingProps> = () => {
-  return <div>Loading</div>;
-};
-
-export default Loading;
